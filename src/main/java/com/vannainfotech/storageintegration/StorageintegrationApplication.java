9601e91250d8bf4d829b400df90964a4894d813c
Bvarun <varunvihar.boddula@vannainfotech.com>
2023-06-28 16:24:51 +0530

    initial commit

---
+++ src/main/java/com/vannainfotech/storageintegration/StorageintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.vannainfotech.storageintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vannainfotech/storageintegration/StorageintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vannainfotech.storageintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
